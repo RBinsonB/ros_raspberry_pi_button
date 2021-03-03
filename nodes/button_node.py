7abff773063b884ebd4d0d2c63fc2b08a3e8b566
RBinsonB <rbaran@kth.se>
2021-03-03 18:30:15 +0100

    Make code functional

--- nodes/button_node.py
+++ nodes/button_node.py
@@ -1,54 +1,56 @@
-# coding=utf-8
+#!/usr/bin/env python
 import rospy
+import sys
 import RPi.GPIO as GPIO
 
 from std_msgs.msg import Bool
 
 class ButtonHandler(object):
     def __init__(self, topic_id, input_pin):
+        self.input_pin = input_pin
         self.status_msg = Bool()
-
+        
         # Setup publisher
-        self.button_pub = rospy.Publisher(topic_id, String, latch=True, queue_size=1)
+        self.button_pub = rospy.Publisher(topic_id, Bool, latch=True, queue_size=1)
 
         # Setup GPIO callback
         try:
             GPIO.setmode(GPIO.BCM)
-            GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.setup(self.input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
             GPIO.add_event_detect(input_pin, GPIO.BOTH, callback=self.button_callback)
+        except:
+            raise ValueError("error in setting up GPIO pin")
 
     # Button callback, call when button is changing state
     def button_callback(self, channel):
-        if GPIO.input(channel) == GPIO.HIGH:
-            self.status_msg.data = True
-        else:
-            self.status_msg.data = False
-
-        self.button_pub.publish()
+        # Set message to GPIO pin value
+        self.status_msg.data = GPIO.input(self.input_pin)
+        # Publish state message
+        self.button_pub.publish(self.status_msg)
 
 
 class ButtonBox():
     def __init__(self):
-    self.button_box_dict = {}
+        self.button_box_dict = {}
 
-    # Get parameters from ROS server
-    self.button_definition = rospy.get_param("~", {})
+        # Get parameters from ROS server
+        self.button_definition = rospy.get_param("~", {})
 
-    # Check params
-    for button in self.button_definition.keys():
-        # For every button, a dictionnary with topic_id and input pin is needed
-        if ('topic_id' in self.button_definition[button].keys()) and ('input_pin' in self.button_definition[button].keys()):
-            # Create the button handler object
-            topic_id = self.button_definition[button]['topic_id']
-            input_pin = self.button_definition[button]['input_pin']
-            self.button_box_dict.update({str(button): ButtonHandler(topic_id, input_pin)})
-            # Log about button
-            rospy.loginfo("%s: connecting button on GPIO %i to topic %s%s" % (rospy.get_name(), input_pin, rospy.get_namespace(), topic_id))
+        # Check params
+        for button in self.button_definition.keys():
+            # For every button, a dictionnary with topic_id and input pin is needed
+            if ('topic_id' in self.button_definition[button].keys()) and ('input_pin' in self.button_definition[button].keys()):
+                # Create the button handler object
+                topic_id = self.button_definition[button]['topic_id']
+                input_pin = self.button_definition[button]['input_pin']
+                self.button_box_dict.update({str(button): ButtonHandler(topic_id, input_pin)})
+                # Log about button
+                rospy.loginfo("%s: connecting button on GPIO %i to topic %s%s" % (rospy.get_name(), input_pin, rospy.get_namespace(), topic_id))
 
-    # If no button has been configured, send warning
-    if not self.button_box_dict:
-        rospy.logerr("%s: button node has been launched but no valid button configuration has been defined" % (rospy.get_name()))
-        raise ValueError("no button config defined")
+        # If no button has been configured, send warning
+        if not self.button_box_dict:
+            rospy.logerr("%s: button node has been launched but no valid button configuration has been defined" % (rospy.get_name()))
+            raise ValueError("no button config defined")
 
 #==============================
 #             Main
